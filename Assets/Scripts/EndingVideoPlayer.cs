012660338d067c9bcd44454f5adca0e14a648039
skawginw <inktwct@gmail.com>
2025-04-10 18:40:16 +0700

    ReimportEnding
    Scene not load in another pc

--- Assets/Scripts/EndingVideoPlayer.cs
+++ Assets/Scripts/EndingVideoPlayer.cs
@@ -9,14 +9,42 @@ public class EndingVideoPlayer : MonoBehaviour
 
     void Start()
     {
+        // Ensure the VideoPlayer is attached
         if (videoPlayer == null)
+        {
             videoPlayer = GetComponent<VideoPlayer>();
+            if (videoPlayer == null)
+            {
+                Debug.LogError("No VideoPlayer found! Please attach a VideoPlayer component.");
+                return;
+            }
+        }
 
+        // Ensure the video is set and ready to play
+        if (videoPlayer.isPrepared)
+        {
+            videoPlayer.Play();
+        }
+        else
+        {
+            videoPlayer.prepareCompleted += PrepareCompleted;
+            videoPlayer.Prepare();
+        }
+
+        // Register the callback for when the video finishes
         videoPlayer.loopPointReached += OnVideoFinished;
     }
 
+    // Called once video has finished playing
     private void OnVideoFinished(VideoPlayer vp)
     {
+        Debug.Log("Video finished! Loading Menu scene.");
         SceneManager.LoadScene(menuSceneName);
+    }
+
+    // Called when video preparation completes
+    private void PrepareCompleted(VideoPlayer vp)
+    {
+        videoPlayer.Play();
     }
 }
